0eb70962771a2a109b89624bbf5050ffed6a8044
Nikita Denisenok <nikitadenisenok@gmail.com>
2023-08-31 08:11:45 +0300

    trash removed

--- internal/service/user/get_segments.go
+++ internal/service/user/get_segments.go
@@ -3,14 +3,5 @@ package user
 import "context"
 
 func (s *Service) GetSegments(ctx context.Context, id int64) ([]string, error) {
-	/* 	var res []string
-	   	segments, err := s.userRepository.GetSegments(ctx, id)
-	   	if err != nil {
-	   		return nil, err
-	   	}
-	   	for _, elem := range segments {
-	   		res = append(res, elem.Slug)
-	   	}
-	   	return res, nil */
 	return s.userRepository.GetSegments(ctx, id)
 }
